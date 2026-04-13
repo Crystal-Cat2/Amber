b00b5a378d639fc8d198eff98f8cf53b079046aa
Amber <549243792@qq.com>
2026-04-13 20:20:47 +0800

    sql: update verify_long_watch_kill_user_kills.sql

--- ad_kill/sql/verify_long_watch_kill_user_kills.sql
+++ ad_kill/sql/verify_long_watch_kill_user_kills.sql
@@ -1,10 +1,12 @@
 -- long_watch_kill 用户在第5关的杀广告次数分布（AB 对比）
--- 验证：B 组跳关是否导致 long_watch_kill 用户杀广告次数更少
+-- 用最后一条事件的 ad_kill_scene 分类（与 dashboard 口径一致）
+-- AB 分组从同事件的 user_properties 提取，不额外扫 user_engagement
 
-WITH user_ab AS (
-  SELECT user_pseudo_id, 'ball_sort' AS product, ab_group
+WITH level5_last_scene AS (
+  SELECT product, user_pseudo_id, ab_group, ad_kill_scene
   FROM (
     SELECT
+      'ball_sort' AS product,
       user_pseudo_id,
       CASE
         WHEN REGEXP_CONTAINS(
@@ -15,20 +17,23 @@ WITH user_ab AS (
           LOWER((SELECT value.string_value FROM UNNEST(user_properties.array) WHERE key = 'game_model')),
           r'(^|[^a-z0-9])(12b)([^a-z0-9]|$)'
         ) THEN 'B'
-      END AS ab_group
+      END AS ab_group,
+      (SELECT ep.value.string_value FROM UNNEST(event_params.array) AS ep WHERE ep.key = 'ad_kill_scene') AS ad_kill_scene,
+      ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp DESC) AS rn
     FROM `transferred.hudi_ods.ball_sort`
     WHERE event_date BETWEEN '2026-01-30' AND '2026-04-07'
-      AND event_name = 'user_engagement'
+      AND event_name IN ('game_new_start', 'game_win')
+      AND (SELECT ep.value.int_value FROM UNNEST(event_params.array) AS ep WHERE ep.key = 'levelid') = 5
       AND user_pseudo_id IS NOT NULL AND user_pseudo_id != ''
   )
-  WHERE ab_group IS NOT NULL
-  GROUP BY user_pseudo_id, ab_group
+  WHERE rn = 1 AND ab_group IS NOT NULL
 
   UNION ALL
 
-  SELECT user_pseudo_id, 'ios_nuts_sort' AS product, ab_group
+  SELECT product, user_pseudo_id, ab_group, ad_kill_scene
   FROM (
     SELECT
+      'ios_nuts_sort' AS product,
       user_pseudo_id,
       CASE
         WHEN REGEXP_CONTAINS(
@@ -39,38 +44,24 @@ WITH user_ab AS (
           LOWER((SELECT value.string_value FROM UNNEST(user_properties.array) WHERE key = 'game_model')),
           r'(^|[^a-z0-9])(2b)([^a-z0-9]|$)'
         ) THEN 'B'
-      END AS ab_group
+      END AS ab_group,
+      (SELECT ep.value.string_value FROM UNNEST(event_params.array) AS ep WHERE ep.key = 'ad_kill_scene') AS ad_kill_scene,
+      ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp DESC) AS rn
     FROM `transferred.hudi_ods.ios_nuts_sort`
     WHERE event_date BETWEEN '2026-02-02' AND '2026-04-07'
-      AND event_name = 'user_engagement'
+      AND event_name IN ('game_new_start', 'game_win')
+      AND (SELECT ep.value.int_value FROM UNNEST(event_params.array) AS ep WHERE ep.key = 'levelid') = 5
       AND user_pseudo_id IS NOT NULL AND user_pseudo_id != ''
   )
-  WHERE ab_group IS NOT NULL
-  GROUP BY user_pseudo_id, ab_group
+  WHERE rn = 1 AND ab_group IS NOT NULL
 ),
 
--- long_watch_kill 用户：第5关 game_new_start 上 ad_kill_scene = 'long_watch_kill'
 long_watch_users AS (
-  SELECT DISTINCT ua.product, ua.ab_group, e.user_pseudo_id
-  FROM `transferred.hudi_ods.ball_sort` e
-  INNER JOIN user_ab ua ON e.user_pseudo_id = ua.user_pseudo_id AND ua.product = 'ball_sort'
-  WHERE e.event_date BETWEEN '2026-01-30' AND '2026-04-07'
-    AND e.event_name = 'game_new_start'
-    AND (SELECT ep.value.int_value FROM UNNEST(e.event_params.array) AS ep WHERE ep.key = 'levelid') = 5
-    AND (SELECT ep.value.string_value FROM UNNEST(e.event_params.array) AS ep WHERE ep.key = 'ad_kill_scene') = 'long_watch_kill'
-
-  UNION ALL
-
-  SELECT DISTINCT ua.product, ua.ab_group, e.user_pseudo_id
-  FROM `transferred.hudi_ods.ios_nuts_sort` e
-  INNER JOIN user_ab ua ON e.user_pseudo_id = ua.user_pseudo_id AND ua.product = 'ios_nuts_sort'
-  WHERE e.event_date BETWEEN '2026-02-02' AND '2026-04-07'
-    AND e.event_name = 'game_new_start'
-    AND (SELECT ep.value.int_value FROM UNNEST(e.event_params.array) AS ep WHERE ep.key = 'levelid') = 5
-    AND (SELECT ep.value.string_value FROM UNNEST(e.event_params.array) AS ep WHERE ep.key = 'ad_kill_scene') = 'long_watch_kill'
+  SELECT product, ab_group, user_pseudo_id
+  FROM level5_last_scene
+  WHERE ad_kill_scene = 'long_watch_kill'
 ),
 
--- 这些用户在第5关的杀广告次数
 user_kill_count AS (
   SELECT lw.product, lw.ab_group, lw.user_pseudo_id, COUNT(*) AS kill_count
   FROM long_watch_users lw
